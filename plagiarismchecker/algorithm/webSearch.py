a5626573a8e6a79945b767d33bc7eac06371c472
Aman <amankasaudhan07@gmail.com>
2024-11-26 22:11:29 +0530

    Algorithm created

--- plagiarismchecker/algorithm/webSearch.py
+++ plagiarismchecker/algorithm/webSearch.py
@@ -4,7 +4,8 @@ from apiclient.discovery import build
 # searchEngine_API = 'AIzaSyAoEYif8sqEYvj1P6vYLw6CGMrQbDMmaq8'
 # searchEngine_API = 'AIzaSyCUYy9AtdMUddiNA0gOcsGPQcE372ytyCw'
 # searchEngine_API = 'AIzaSyAQYLRBBeDQNxADPQtUnApntz78-urWEZI'
-searchEngine_API = 'AIzaSyCAeR7_6TTKzoJmSwmOuHZvKcVg_lhqvCc'
+searchEngine_API = 'AIzaSyBnPQ_Lbj3wb77PAbasgESvqqr0-Ay32F4'
+# searchEngine_API = 'AIzaSyCAeR7_6TTKzoJmSwmOuHZvKcVg_lhqvCc'
 searchEngine_Id = '758ad3e78879f0e08'
 
 def searchWeb(text, output, c):
